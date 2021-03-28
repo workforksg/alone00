1de9b71fc0247b04193415d68cb22227bc0521e9
ksg <workforksg@gmail.com>
2021-03-29 00:35:44 +0900

    igxx

---
+++ src/test/java/com/alone/my/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.alone.my.springboot.web.dto;
+
+import com.alone.my.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+        System.out.println("악....시바라루짱시바라루짱시바라루짱시바라루짱");
+    }
+}
