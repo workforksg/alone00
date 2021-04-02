10bf18f0528d64d2bb42cde2b54c955cdffbc6f6
ksg <workforksg@gmail.com>
2021-04-03 05:50:11 +0900

    fuck

--- src/main/java/com/alone/my/springboot/web/ProfileController.java
+++ src/main/java/com/alone/my/springboot/web/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
